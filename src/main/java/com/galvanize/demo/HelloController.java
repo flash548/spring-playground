c6cea9760f4538a85d5970f5f4d4a36283ec83eb
Christopher Zell <christopher.zell@gmail.com>
2020-07-20 15:05:23 -0400

    with area - object

--- src/main/java/com/galvanize/demo/HelloController.java
+++ src/main/java/com/galvanize/demo/HelloController.java
@@ -1,9 +1,30 @@
 package com.galvanize.demo;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 @RestController
 public class HelloController {
+
+
+    public class AreaParams {
+
+        private String type="None";
+        private String height="None";
+        private String width="None";
+        private String radius="None";
+
+        public String getType() { return this.type; }
+        public String getHeight() { return this.height; }
+        public String getWidth() { return this.width; }
+        public String getRadius() { return this.radius; }
+
+        public void setType(String t) { this.type = t; }
+        public void setHeight(String h) { this.height = h; }
+        public void setWidth(String w) { this.width = w; }
+        public void setRadius(String r) { this.radius = r; }
+    }
+
     @GetMapping("/math/pi")
     public String getPI() {
         return "3.141592653589793";
@@ -36,4 +57,20 @@ public class HelloController {
         return String.valueOf(x*y*z);
     }
 
+    @PostMapping("/math/area")
+    public String getArea(AreaParams params) {
+        switch(params.type) {
+            case "rectangle":
+                if (!params.height.equals("None") && !params.width.equals("None"))
+                    return "Area of a " + params.width + "x" + params.height + " rectangle is " + String.valueOf(Double.parseDouble(params.width) * Double.parseDouble(params.height));
+                else
+                    return "Invalid";
+            case "circle":
+                if (!params.radius.equals("None"))
+                    return "Area of a circle with radius of " + params.radius + " is " + String.valueOf(Math.PI * Math.pow(Double.parseDouble(params.radius), 2));
+                else
+                    return "Invalid";
+            default: return "Invalid";
+        }
+    }
 }
