012042c7e27df6994d07a50b7b2f26236fb4de9a
Christopher Zell <christopher.zell@gmail.com>
2020-07-22 15:45:26 -0400

    with READ and DELETE of CRUD

--- src/main/java/com/galvanize/demo/LessonsController.java
+++ src/main/java/com/galvanize/demo/LessonsController.java
@@ -2,6 +2,8 @@ package com.galvanize.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
@@ -21,4 +23,25 @@ public class LessonsController {
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
     }
+    @GetMapping("/{id}")
+    public Lesson getId(@PathVariable String id) {
+        Optional<Lesson> retVal = this.repository.findById(Long.valueOf(id));
+        if (retVal.isPresent()) {
+            return retVal.get();
+        }
+        else {
+            return new Lesson();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteId(@PathVariable String id) {
+        try {
+            this.repository.deleteById(Long.valueOf(id));
+        }
+        catch (Exception e) {}
+
+        return "Done";
+    }
+
 }
